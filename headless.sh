642574dbfedf4e9d01444b6bd4593b12fb447885
Simon Priadka <priadkasimon@gmail.com>
2016-02-10 15:34:09 +0100

    changed headless

--- headless.sh
+++ headless.sh
@@ -2,6 +2,7 @@
 
 Xvfb :1 -screen 0 1024x768x8 &
 export DISPLAY=:1.5
-java -jar selenium-server-standalone-*.jar
+java -jar $HOME/selenium-server-standalone-*.jar
 mvn clean verify -Pvisual-testing -Dtest=AnotherTest -Dintegration=wildfly82-remote
-
+killall Xvfb
+killall java
