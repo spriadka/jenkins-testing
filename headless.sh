6b9680158a4a71fd06b61a99b168e94ebee7c052
Simon Priadka <priadkasimon@gmail.com>
2016-02-10 15:43:57 +0100

    changed headless

--- headless.sh
+++ headless.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
 Xvfb :1 -screen 0 1024x768x8 &
+XVFB=$!
 export DISPLAY=:1.5
-java -jar $HOME/selenium-server-standalone-*.jar
+java -jar $HOME/selenium-server-standalone-*.jar &
+SELENIUM=$!
 mvn clean verify -Pvisual-testing -Dtest=AnotherTest -Dintegration=wildfly82-remote
-killall Xvfb
-killall java
+kill $XVFB
+kill $SELENIUM
