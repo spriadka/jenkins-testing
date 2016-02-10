0bb60e05a32398afe5ba96a45a372230a110ba5b
Simon Priadka <priadkasimon@gmail.com>
2016-02-10 18:03:10 +0100

    got headless working

--- headless.sh
+++ headless.sh
@@ -2,8 +2,5 @@
 Xvfb :99 -screen 5 1024x768x8 &
 XVFB=$!
 export DISPLAY=':99.5'
-java -jar $HOME/selenium-server-standalone-*.jar &
-SELENIUM=$!
 mvn clean verify -Pvisual-testing -Dtest=AnotherTest -Dintegration=wildfly82-remote
 kill $XVFB
-kill $SELENIUM
