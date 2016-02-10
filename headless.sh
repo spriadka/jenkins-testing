8ebe28fab9efd1a1a3d1a0695a1ecafde587791a
Simon Priadka <priadkasimon@gmail.com>
2016-02-10 17:36:09 +0100

    another change for headless

--- headless.sh
+++ headless.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-Xvfb :1 -screen 5 1024x768x8 &
+Xvfb :99 -screen 5 1024x768x8 &
 XVFB=$!
+export DISPLAY=':99.5'
 java -jar $HOME/selenium-server-standalone-*.jar &
 SELENIUM=$!
 mvn clean verify -Pvisual-testing -Dtest=AnotherTest -Dintegration=wildfly82-remote
